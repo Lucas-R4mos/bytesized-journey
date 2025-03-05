15bb1f0723803462a13d16058810b18bf7174d5a
Lucas da Hora Ramos <lucasr4mos_@hotmail.com>
2025-03-05 12:52:52 -0300

    feat(userManagement): include salt based hash mechanism

--- src/userManagement/config.py
+++ src/userManagement/config.py
@@ -7,4 +7,6 @@ load_dotenv()
 class Config(object):
     TESTING = False
     DEBUG = False
-    SECRET_KEY = environ.get('SECRET_KEY', None)
+    SECRET_KEY = environ.get('USR_MNG_SECRET_KEY', None)
+    PASSWORD_SALT = environ.get(
+        'USR_MNG_PASSWORD_SALT', None)
